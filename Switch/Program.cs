5ce893a3b873dfb2e32e1a26908e33db9cfa898a
Sripriya Sundararaman <spreiyah@yahoo.com>
2021-06-11 16:11:53 +1200

    Add project files.

---
+++ Switch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Enter number between 1-4");
+            a = Convert.ToInt32(Console.ReadLine());
+            switch(a)
+            {
+                case 1:
+                
+                    Console.WriteLine("You chose number 1");
+                break;
+                case 2:
+                
+                    Console.WriteLine("You chose number 2");
+                    break;
+                
+                case 3:
+                
+                    Console.WriteLine("You chose number 4");
+                    break;
+
+            case 4:
+                
+                Console.WriteLine("You chose number 4");
+                break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+        }
+        }
+    }
+
